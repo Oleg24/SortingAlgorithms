8b2c6c147a3df21da7482944b010cfef69997036
Oleg <o.yanchinskiy@gmail.com>
2014-12-31 11:00:30 -0800

    selection sort implementation in java

---
+++ selectionSort.java
@@ -0,0 +1,30 @@
+public class selectionSort{
+  public static void main(String[] args){
+    int []numbers = {10, 5, 3, 7, 2};
+
+    sort(numbers);
+
+    printArray(numbers);
+  }
+
+  public static int[] sort(int[] A){
+    for(int i=0; i<A.length-1; i++){
+      int minIndex = i;
+      for(int j= i+1; j < A.length; j++){
+        if(A[j] < A[minIndex]){
+          minIndex = j;
+        }
+      }
+      int temp = A[minIndex];
+      A[minIndex] = A[i];
+      A[i] = temp;
+    }
+    return A;
+  }
+
+  public static void printArray(int[] A){
+    for(int i = 0; i<A.length; i++){
+      System.out.println(A[i]);
+    }
+  }
+}
